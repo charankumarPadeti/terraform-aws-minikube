f222a5d2b8e007e630ec9cb671b117ddb2392430
charankumar07 <charankumar0999@gmail.com>
2024-12-01 07:26:54 +0530

    Practicing

--- docker.sh
+++ docker.sh
@@ -56,10 +56,12 @@ VALIDATE $? "added centos user to docker group"
 
 echo -e "$R Logout and login again $N"
 
+
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
 
 chmod +x kubectl
 
-mv kubectl /usr/local/bin/kubetl
+mv kubectl /usr/local/bin/kubectl
 
-VALIDATE $? "kubectl installation"
+VALIDATE $? "Kubectl installation"
